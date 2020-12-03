6d00820c0e329ead389c29926d2b5254dc60cc6c
Thomas Drumont <thomas.drumont@epitech.eu>
2020-12-03 18:45:27 +0100

    comments my work :tada:

--- src/my_merge_image.php
+++ src/my_merge_image.php
@@ -11,6 +11,7 @@ function my_merge_image($scan, $name)
         }
     }
 
+    // default value
     $width = 0;
     $height = 0;
     $pos = 0;
@@ -21,37 +22,44 @@ function my_merge_image($scan, $name)
     foreach ($images as $image) {
         $infos = getimagesize($image);
         if (isset($infos)) {
+            // get image size
             list($width, $height) = $infos;
         }
 
+        // put image and size into array
         $tmpFile[$image] = [
             "width" => $width,
             "height" => $height
         ];
-//
-//        dump($tmpFile);
 
+        // get total width (for sprite image)
         $totalWidth += $width;
+        // get biggest height (for sprite image)
         $biggestHeight = max($tmpFile)['height'];
     }
 
-    // create empty image
+    // create empty image with default background etc
     $spriteImg = imagecreatetruecolor($totalWidth, $biggestHeight);
     $background = imagecolorallocatealpha($spriteImg, 255, 255, 255, 127);
     imagefill($spriteImg, 0, 0, $background);
     imagealphablending($spriteImg, false);
     imagesavealpha($spriteImg, true);
 
+    // for each file into the array with file and sizes
     foreach ($tmpFile as $image => $size) {
+        // get image data
         $tempImg = imagecreatefrompng($image);
+
+        // copy the image into the default sprite image
         imagecopy($spriteImg, $tempImg, $pos, 0, 0, 0, $size['width'], $size['height']);
 
+        // spacing images
         $pos += $size['width'];
         imagedestroy($tempImg);
     }
 
+    // create the sprite image with name passed in parameter
     imagepng($spriteImg, $name);
-
 }
 
 function mergeIntoSingle(array $array)
